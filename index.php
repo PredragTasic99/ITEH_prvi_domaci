4c961902ba61971943ac427a4ac4c8bed32b3607
PredragTasic99 <predrag.tasic99@gmail.com>
2022-01-26 19:30:02 +0100

    ubacen style.css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Automobili.com</title>
-  <link rel="stylesheet" href="global.css">
+  <link rel="stylesheet" href="style.css">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
     crossorigin="anonymous"></script>
